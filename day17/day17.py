b746c1c92dafcdc58fc992b146e5928ccf343752
SpeckledFleebeedoo <datema150@gmail.com>
2022-12-18 01:42:03 +0100

    Day 17

--- day17/day17.py
+++ day17/day17.py
@@ -3,7 +3,6 @@ import numpy as np
 #Read data
 with open("day17/input.txt") as f:
     instructions = f.read()
-print(len(instructions))
 
 #Define sequence of rocks
 rocks = [
@@ -27,11 +26,13 @@ map = np.array([[1,1,1,1,1,1,1,1,1]])
 
 i = 0   #Instruction counter
 totalheight = 0
+knownstates = []
+heights = []
 
-for r in range(1000000000000):
+for r in range(2022):
     rock = rocks[r%5]
-    if r%1000==0:
-        print(r)
+    # if r%1000==0:
+    #     print(r)
 
     #Space between current map and new rock
     addon = np.array([[1,0,0,0,0,0,0,0,1],
@@ -50,6 +51,7 @@ for r in range(1000000000000):
     touchdown = False
     
     #Stack existing map, empty space and rock
+    rowsadded = len(rockaddon) + len(addon)
     map = np.concatenate((rockaddon, addon, map), axis=0)
     while not touchdown:
         #Read current instruction
@@ -70,21 +72,44 @@ for r in range(1000000000000):
 
         #Repeat for moving down
         trycoords = (rockpos[0]+1, rockpos[1])
-        testmap = map[0:50].copy()
+        testmap = map.copy()
         testmap[trycoords[0]:trycoords[0]+height, trycoords[1]:trycoords[1]+width] += rock
         if not 2 in testmap:
             rockpos = trycoords
 
         #Place landed rock in map, strip empty lines from top of map
         else:
-            touchdown = True
             map[rockpos[0]:rockpos[0]+height, rockpos[1]:rockpos[1]+width] += rock
-            for row in map:
-                if np.count_nonzero(row) == 2:
-                    map = np.delete(map, 0, 0)
-        toremove = len(map) - 50
-        if toremove > 0:
-            map = np.delete(map, np.s_[51:], 0)
-        totalheight += toremove
+            touchdown = True
 
-print(map.shape[0]-1+totalheight)   #Correct for floor...
+    #Trim map
+    for y, row in enumerate(map):
+        if np.count_nonzero(row) != 2:
+            rowsremoved = y
+            break 
+    map = np.delete(map, np.s_[:y], 0)
+
+    totalheight += (rowsadded - rowsremoved)
+    heights.append(totalheight)
+
+    #Calculate state
+    s1, s2, s3, s4, s5, s6, s7 = [np.where(map[:,i] == 1)[0][0] for i in range(1, 8)]
+    state = [i%len(instructions), r%len(rocks), s1, s2, s3, s4, s5, s6, s7]
+    if state in knownstates:
+        index = knownstates.index(state)
+        break
+
+    #Trim bottom of map
+    knownstates.append(state)
+    lowest = max(s1, s2, s3, s4, s5, s6, s7)
+    map = np.delete(map, np.s_[lowest+21:], 0)
+
+preloopheight = heights[index]
+loopheight = totalheight - preloopheight
+rocksinloop = r - index
+loopstofill = (1000000000000 - index) // rocksinloop
+rocksremaining = (1000000000000 - index) % rocksinloop
+loopheight = loopstofill * loopheight
+totalheight = heights[index + rocksremaining] + loopheight - 1
+
+print(totalheight)
